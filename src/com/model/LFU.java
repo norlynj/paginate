e741bf8781a301dbd722c53652eb9e1d435e8016
Jannah <jcesplanada1@up.edu.ph>
2023-05-18 01:59:18 +0800

    lfu and mfu algos

--- src/com/model/LFU.java
+++ src/com/model/LFU.java
@@ -1,8 +1,64 @@
 package model;
 
-public class LFU extends PageReplacementSimulator{
+import java.util.*;
+
+public class LFU extends PageReplacementSimulator {
     public LFU(PageReferenceString prs, int frameNum) {
         super(prs);
         this.frameNumber = frameNum;
+    }
+
+    public void simulate() {
+        steps = new ArrayList<>();
+        String status = "";
+        int currentFrame = 0;
+
+        ArrayList<Integer> s = new ArrayList<>(frameNumber);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        ArrayList<Integer> pages = string.getPages();
+        int pf = 0;
+        for (int i = 0; i < pages.size(); i++) {
+            if (s.size() < frameNumber) {
+                if (!s.contains(pages.get(i))) {
+                    s.add(pages.get(i));
+                    freq.put(pages.get(i), 1);
+                    pf++;
+                    status = "miss";
+                    currentFrame = s.size();
+                } else {
+                    freq.put(pages.get(i), freq.get(pages.get(i)) + 1);
+                    status = "hit";
+                }
+            } else {
+                if (!s.contains(pages.get(i))) {
+                    int minFreq = Collections.min(freq.values());
+                    ArrayList<Integer> leastFrequentPages = new ArrayList<>();
+                    for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+                        if (entry.getValue() == minFreq) {
+                            leastFrequentPages.add(entry.getKey());
+                        }
+                    }
+                    int lfuPage = leastFrequentPages.get(0);
+                    for (int j = 1; j < leastFrequentPages.size(); j++) {
+                        if (s.indexOf(leastFrequentPages.get(j)) < s.indexOf(lfuPage)) {
+                            lfuPage = leastFrequentPages.get(j);
+                        }
+                    }
+                    currentFrame = s.indexOf(lfuPage);
+                    s.set(currentFrame, pages.get(i));
+                    freq.remove(lfuPage);
+                    freq.put(pages.get(i), 1);
+
+                    // Increment page faults
+                    pf++;
+                    status = "miss";
+                } else {
+                    freq.put(pages.get(i), freq.get(pages.get(i)) + 1);
+                    status = "hit";
+                }
+            }
+            steps.add(new Step(i, currentFrame, status, new ArrayList<>(s), pf));
+        }
+        this.pageFaults = pf;
     }
 }
--- src/com/model/MFU.java
+++ src/com/model/MFU.java
@@ -1,12 +1,64 @@
 package model;
 
-public class MFU extends PageReplacementSimulator{
+import java.util.*;
+
+public class MFU extends PageReplacementSimulator {
     public MFU(PageReferenceString prs, int frameNum) {
         super(prs);
         this.frameNumber = frameNum;
     }
 
     public void simulate() {
+        steps = new ArrayList<>();
+        String status = "";
+        int currentFrame = 0;
 
+        ArrayList<Integer> s = new ArrayList<>(frameNumber);
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        ArrayList<Integer> pages = string.getPages();
+        int pf = 0;
+        for (int i = 0; i < pages.size(); i++) {
+            if (s.size() < frameNumber) {
+                if (!s.contains(pages.get(i))) {
+                    s.add(pages.get(i));
+                    freq.put(pages.get(i), 1);
+                    pf++;
+                    status = "miss";
+                    currentFrame = s.size();
+                } else {
+                    freq.put(pages.get(i), freq.get(pages.get(i)) + 1);
+                    status = "hit";
+                }
+            } else {
+                if (!s.contains(pages.get(i))) {
+                    int maxFreq = Collections.max(freq.values());
+                    ArrayList<Integer> mostFrequentPages = new ArrayList<>();
+                    for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+                        if (entry.getValue() == maxFreq) {
+                            mostFrequentPages.add(entry.getKey());
+                        }
+                    }
+                    int mfuPage = mostFrequentPages.get(0);
+                    for (int j = 1; j < mostFrequentPages.size(); j++) {
+                        if (s.indexOf(mostFrequentPages.get(j)) < s.indexOf(mfuPage)) {
+                            mfuPage = mostFrequentPages.get(j);
+                        }
+                    }
+                    currentFrame = s.indexOf(mfuPage);
+                    s.set(currentFrame, pages.get(i));
+                    freq.remove(mfuPage);
+                    freq.put(pages.get(i), 1);
+
+                    // Increment page faults
+                    pf++;
+                    status = "miss";
+                } else {
+                    freq.put(pages.get(i), freq.get(pages.get(i)) + 1);
+                    status = "hit";
+                }
+            }
+            steps.add(new Step(i, currentFrame, status, new ArrayList<>(s), pf));
+        }
+        this.pageFaults = pf;
     }
 }
